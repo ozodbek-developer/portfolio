df11beadfe0a969167c5740ef11e50e179165e61
ozodbek-developer <ozodbekyusupov.developer@gmail.com>
2021-12-05 17:58:13 +0500

    :)

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+        
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,40 +15,36 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
+    
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
+    
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Freelancer", "Designer", "SEO Manager"],
+        strings: ["Web dasturchiman", "Mobile dasturchiman", "Frilanserman", "Dizaynerman"],
         typeSpeed: 30,
         backSpeed: 40,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Freelancer", "Designer", "SEO Manager"],
+        strings: ["Web dasturchiman", "Mobile dasturchiman", "Frilanserman", "Dizaynerman"],
         typeSpeed: 30,
         backSpeed: 40,
         loop: true
     });
 
-    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
